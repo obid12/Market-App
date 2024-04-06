b663ef97d6322ade84d2132f51a99bb87dc194fa
obid12 <obidiaramdhani23@gmail.com>
2024-04-06 11:14:37 +0700

    test

--- app/src/main/java/com/obidia/marketapp/MainActivity.kt
+++ app/src/main/java/com/obidia/marketapp/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.obidia.marketapp.ui.theme.MarketAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,26 +15,13 @@ class MainActivity : ComponentActivity() {
     setContent {
       MarketAppTheme {
         // A surface container using the 'background' color from the theme
-        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-          Greeting("Android")
+        Surface(
+          modifier = Modifier.fillMaxSize(),
+          color = MaterialTheme.colorScheme.background
+        ) {
+
         }
       }
     }
-  }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-  Text(
-    text = "Hello $name!",
-    modifier = modifier
-  )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-  MarketAppTheme {
-    Greeting("Android")
   }
 }
